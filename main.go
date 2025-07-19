86567a9c94a13a099b7e4b894ef011aec51181eb
Adam0x59 <4dam.5.m@gmail.com>
2025-07-19 13:00:21 +0100

    Added index.html and handler to serve the file from root dir

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"net/http"
 )
 
-type ServeMux struct {
-}
-
 func main() {
+	//Config
 	const port = "8080"
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	//Server
 	server := &http.Server{Addr: ":" + port, Handler: mux}
 	log.Printf("Serving on port: %q\n", port)
 	log.Fatal(server.ListenAndServe())
